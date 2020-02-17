0ab0da62ea87af62731084ecd14894f60291b58c
Kyle Chou <kyle@KyledeMacBook-Pro.local>
2020-02-17 16:39:49 +0800

    add consul & es sh

---
+++ work/consul/consul.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 中间件目录
+MIDDLEWAREMIDDLEWARE_DIR=/data
+HOST_IP=$(ip address show |grep global|grep eth0|awk -F '[/ ]' '{print $6}')
+
+# 创建中间件目录
+mkdir -p /$MIDDLEWAREMIDDLEWARE_DIR/consul
+
+cp docker-compose.yml $MIDDLEWAREMIDDLEWARE_DIR/consul
+
+cd /$MIDDLEWAREMIDDLEWARE_DIR/consul && /usr/bin/docker-compose up -d
