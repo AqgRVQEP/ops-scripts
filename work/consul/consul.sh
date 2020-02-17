4e10d0871cc6d1527d493956f7eaa2d2cd979465
Kyle Chou <kyle@KyledeMacBook-Pro.local>
2020-02-17 16:48:11 +0800

    update sh add sudo & exit

--- work/consul/consul.sh
+++ work/consul/consul.sh
@@ -5,8 +5,10 @@ MIDDLEWAREMIDDLEWARE_DIR=/data
 HOST_IP=$(ip address show |grep global|grep eth0|awk -F '[/ ]' '{print $6}')
 
 # 创建中间件目录
-mkdir -p /$MIDDLEWAREMIDDLEWARE_DIR/consul
+sudo mkdir -p /$MIDDLEWAREMIDDLEWARE_DIR/consul
 
-cp docker-compose.yml $MIDDLEWAREMIDDLEWARE_DIR/consul
+sudo cp docker-compose.yml $MIDDLEWAREMIDDLEWARE_DIR/consul
 
 cd /$MIDDLEWAREMIDDLEWARE_DIR/consul && /usr/bin/docker-compose up -d
+
+exit
--- work/elasticsearch/es.sh
+++ work/elasticsearch/es.sh
@@ -5,8 +5,10 @@ MIDDLEWAREMIDDLEWARE_DIR=/data
 HOST_IP=$(ip address show |grep global|grep eth0|awk -F '[/ ]' '{print $6}')
 
 # 创建中间件目录
-mkdir -p /$MIDDLEWAREMIDDLEWARE_DIR/elasticsearch
+sudo mkdir -p /$MIDDLEWAREMIDDLEWARE_DIR/elasticsearch
 
-cp docker-compose.yml $MIDDLEWAREMIDDLEWARE_DIR/elasticsearch
+sudo cp docker-compose.yml $MIDDLEWAREMIDDLEWARE_DIR/elasticsearch
 
 cd /$MIDDLEWAREMIDDLEWARE_DIR/elasticsearch && /usr/bin/docker-compose up -d
+
+exit
