124590cdfdb308ccfa08957aae06732abc6ec283
Kyle Chou <kyle@KyledeMacBook-Pro.local>
2020-02-17 17:40:15 +0800

    update cp .env to work dir

--- work/consul/consul.sh
+++ work/consul/consul.sh
@@ -3,13 +3,11 @@
 # 中间件目录
 MIDDLEWAREMIDDLEWARE_DIR=/data
 
-IP=$(ip address show |grep global|grep eth0|awk -F '[/ ]' '{print $6}')
-echo "HOST_IP=${IP}" > .env
-
 # 创建中间件目录
 sudo mkdir -p /$MIDDLEWAREMIDDLEWARE_DIR/consul
 
 sudo cp docker-compose.yml $MIDDLEWAREMIDDLEWARE_DIR/consul
+sudo cp .env $MIDDLEWAREMIDDLEWARE_DIR/consul
 
 cd /$MIDDLEWAREMIDDLEWARE_DIR/consul && /usr/bin/docker-compose up -d
 
